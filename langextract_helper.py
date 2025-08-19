9bde200f1658ddda68e644b5a0eb6319471babf3
Ojuka <cliffordojuka53@gmail.com>
2025-08-19 15:06:25 +0300

    modified langextract_helper

--- langextract_helper.py
+++ langextract_helper.py
@@ -19,7 +19,7 @@ def get_transcript(video_url: str) -> str:
         video_id = video_url.split("/")[-1]
 
     # Create instance of YouTubeTranscriptApi
-    transcript = YouTubeTranscriptApi().get_transcript(video_id)
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
     return " ".join([t["text"] for t in transcript])
 
 def chunk_text(text: str, chunk_size=1000, overlap=100):
